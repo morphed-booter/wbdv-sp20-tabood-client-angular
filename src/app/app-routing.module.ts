baa16e8344bec98068ec8489e133488b30bfa8cb
tabood <abood.t@husky.neu.edu>
2020-04-21 16:20:15 -0400

    Added routing for future

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {CourseTableComponent} from './components/course-table/course-table.component';
+import {CourseViewerComponent} from './components/course-viewer/course-viewer.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: CourseTableComponent},
+  {path: 'courses', component: CourseTableComponent},
+  {path: 'course/:cid', component: CourseViewerComponent},
+  {path: 'course/:cid/module/:mid', component: CourseViewerComponent},
+  {path: 'course/:cid/module/:mid/lesson/:lid', component: CourseViewerComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
