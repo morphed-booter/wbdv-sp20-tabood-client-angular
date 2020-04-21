a9da4794adaa321aba2a1cb19c4d23f44c7ec2fd
tabood <abood.t@husky.neu.edu>
2020-04-21 14:34:45 -0400

    Created initial components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,23 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CourseListComponent } from './course-list/course-list.component';
+import { CourseListComponent } from './components/course-list/course-list.component';
 import {CourseServiceClient} from './services/CourseServiceClient';
 import {LessonServiceClient} from './services/LessonServiceClient';
 import {ModuleServiceClient} from './services/ModuleServiceClient';
+import { CourseTableComponent } from './components/course-table/course-table.component';
+import { CourseViewerComponent } from './components/course-viewer/course-viewer.component';
+import { ModuleListComponent } from './components/module-list/module-list.component';
+import { LessonTabsComponent } from './components/lesson-tabs/lesson-tabs.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CourseListComponent
+    CourseListComponent,
+    CourseTableComponent,
+    CourseViewerComponent,
+    ModuleListComponent,
+    LessonTabsComponent,
   ],
   imports: [
     BrowserModule,
